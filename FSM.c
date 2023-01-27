7130fc94579fb4ad8e5f2d09accdecf7353a0c4f
jacquii99 <jmendez21@alumnos.uaq.mx>
2023-01-26 20:19:55 -0600

    ya casi

--- FSM.c
+++ FSM.c
@@ -5,45 +5,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "math.h"
+#define sensor(*((volatile unit32_t*)0x4002400C))
+#define luz (*((volatile unit32_t*)0x400050FC))
+
 
 struct estado {
   uint32_t Out;
   uint32_t Time; 
   uint32_t Next[4];};
-
-volatile unsigned long LED;
-int main (void)
-{
-  Configurar_PLL(); //configuracion de velocidad de reloj
-  Configurar_GPIO();
-  while(1)
-  {
-
-    LED = 0X01;
-    GPION -> DATA=LED;
-    Delay();
-    LED = 0x02; 
-    GPION -> DATA=LED;
-    Delay ();
-    LED = 0X04;
-    GPION -> DATA=LED;
-    Delay();
-    LED = 0x08; 
-    GPION -> DATA=LED;
-    Delay ();
-
-  }
-}
-
-
 typedef const struct estado State_t;
 #define N   0
 #define EN 1
 #define E   2
 #define EE 3
-uint32_t S;  
-uint32_t Input;
-
 State_t FSM[4]=
 {
  {0x21,3000,{N,EN,N,EN}},
@@ -51,3 +25,23 @@ State_t FSM[4]=
  {0x0C,3000,{E,E,EE,EE}},
  {0x14, 500,{N,N,N,N}}
  };
+
+uint32_t S;  //para el estado actual
+uint32_t Input; //entrada 
+
+int main (void){
+Configurar_PLL(); //configuracion de velocidad de reloj
+Configurar_GPIO();
+SysTick_Init();
+  
+  S=  N; 
+  while(1)
+  {
+    luz=FSM[S].Out;
+    Input=sensor; 
+    S= FSM[S].Next[Input]; 
+    SysTick_Wait10ms(FSM[S].Time)
+  }
+}
+
+
