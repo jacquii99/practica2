42ab87839a132f9b6102be550b4dfb1b6cca4c52
jacquii99 <jmendez21@alumnos.uaq.mx>
2023-01-28 00:53:56 -0600

    listo

--- GPIO.c
+++ GPIO.c
@@ -7,8 +7,18 @@ extern void Configurar_GPIO(void)
 volatile unit32_t delay = SYSCTL->RCGCGPIO;
                    // B        E
 SYSCTL->RCGCGPIO |= (1<<1) | (1<<4) ;
-GPIOE -> DIR &=~0x03; //deshabilita el pin E como entrada 
-GPIOE -> DEN |= 0x03;//habilta el pin E como digital
-GPIOB -> DIR |= 0X3F; //habilita el pin B como salida 
-GPIOB -> DEN |= 0X3F; //habilita el pin B como digital 
+
+GPIOE_AHB -> DIR &=~0x03; //deshabilita el pin E como entrada 
+GPIOE_AHB -> DEN |= 0x03;//habilta el pin E como digital
+GPIOB_AHB -> DIR |= 0X3F; //habilita el pin B como salida 
+GPIOB_AHB -> DEN |= 0X3F; //habilita el pin B como digital 
+}
+extern void Delay (void)
+{
+    unsigned long volatile time; 
+    time=16000000; 
+    while(time)
+    {
+        time--; 
+    }
 }
